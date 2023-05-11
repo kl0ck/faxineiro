517a94269d6f962b548c0fca196fa83cdee20127
kl0ck <kl0ck@users.noreply.github.com>
2023-05-11 19:27:32 -0300

    Primeiro commit

---
+++ faxineiro.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+__descricao = "Script para remoção de linhas de código inúteis."
+
+def main(inputFileName):
+    if not os.path.exists(inputFileName):
+        print("error: Arquivo", inputFileName, "não existe!")
+        sys.exit()
+
+    print("Processando... \n")
+
+    inputFile = open(inputFileName, "r")
+
+    inputLines = inputFile.readlines()
+    
+    outputLines = []
+    
+    removeFile = open("faxineiro-linhas-remover.txt", "r")
+    
+    removeLines = removeFile.readlines()
+    
+    removeSet = set()
+    
+    for removeLine in removeLines:
+        removeSet.add(removeLine.strip())
+
+    for line in inputLines:
+        filtrarLinha(line, removeSet, outputLines)
+
+    outputFile = open(inputFileName + ".limpo", "w")
+    
+    outputFile.writelines(outputLines)
+    
+    inputFile.close()
+    outputFile.close()
+    
+    print("Pronto! \n")
+    
+    print("outputFile =", outputFile.name, "\n")
+    
+def filtrarLinha(line, removeSet, outputLines):
+    if (line.strip() not in removeSet):
+        outputLines.append(line)
+    
+if __name__ == "__main__":
+    # https://docs.python.org/3/howto/argparse.html
+    import argparse
+
+    parser = argparse.ArgumentParser(description = __descricao)
+    
+    parser.add_argument('-inputFile', required=True, help='arquivo com o código a limpar')
+    
+    args = parser.parse_args()
+    
+    print()
+    print(__descricao, "\n")
+    
+    print()
+    print("inputFile =", args.inputFile, "\n")
+    
+    main(args.inputFile)
